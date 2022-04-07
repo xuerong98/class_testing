3f22439266ce3beb768284240d648dedcda2a739
xuerong98 <xuerongs98@gmail.com>
2022-04-07 13:45:43 -0500

    Update class_testing.py
    work for question 1

--- class_testing.py
+++ class_testing.py
@@ -6,3 +6,9 @@ This is a temporary script file.
 """
 
 print('hello world!')
+
+my_list = ['a', 'b', 'c', 'd', 'e']
+i = 0
+for letter in my_list:
+    i += 1
+    print('The value at position', i, 'is', letter)
